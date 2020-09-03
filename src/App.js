3c4afe428e5619fef2d48c9368af6dfc873b5faa
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-09-02 20:52:28 -0700

    working on aligning grid

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MovieList from "./MovieList";
 import Input from "./Input";
 import useNominations from "./hooks/useNominations";
 import NominatedMovies from "./NominatedMovies";
+import Grid from "@material-ui/core/Grid";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
@@ -11,16 +12,42 @@ export default function App() {
 
   return (
     <>
-      <Input setMovies={setMovies}></Input>
-      <MovieList
-        movies={movies}
-        nominations={nominations}
-        nominateMovie={nominateMovie}
-      ></MovieList>
-      <NominatedMovies
-        movies={nominations}
-        removeNomination={removeNomination}
-      />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          <Input setMovies={setMovies}></Input>
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        spacing={0}
+        direction="row"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={6}>
+          <h1 text-align="center">Movie List:</h1>
+          <MovieList
+            movies={movies}
+            nominations={nominations}
+            nominateMovie={nominateMovie}
+          ></MovieList>
+        </Grid>
+        <Grid item xs={6}>
+          <h1>Nominated Movies:</h1>
+          <NominatedMovies
+            movies={nominations}
+            removeNomination={removeNomination}
+          />
+        </Grid>
+      </Grid>
     </>
   );
 }
