539fd00e8859acd1504bc2cc84e825dcdc98dde4
joshbrookstone <joshbrookstone@gmail.com>
2020-09-04 17:29:03 -0700

    Updates

---
+++ precache-manifest.5ba8b5e3767dbda6ceb6782490183ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd580b3b1c01b2707508ab979aa84c91",
+    "url": "/shopifyux/index.html"
+  },
+  {
+    "revision": "d4a03060bdd9b4f4a432",
+    "url": "/shopifyux/static/css/main.8dacf295.chunk.css"
+  },
+  {
+    "revision": "4c40e344bc1c91368529",
+    "url": "/shopifyux/static/js/2.5e18f19d.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/shopifyux/static/js/2.5e18f19d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4a03060bdd9b4f4a432",
+    "url": "/shopifyux/static/js/main.b94fb8fb.chunk.js"
+  },
+  {
+    "revision": "3b1fbb19fdb13cafaa30",
+    "url": "/shopifyux/static/js/runtime-main.c5ed0219.js"
+  }
+]);
